687e3dd43f8724c6a636de1afd62db2e292e7afa
Divya Premanantha <premananthadivya@gmail.com>
2024-10-03 14:03:16 +0530

    Add organization creation logic

--- src/routes/adminRoute.js
+++ src/routes/adminRoute.js
@@ -4,5 +4,7 @@ const adminService = require('../services/adminService');
 
 router.post('/organisation', adminService.createOrganization);
 router.get('/organisation', adminService.getOrganization);
+router.put('/organisation', adminService.updateOrganization);
+router.delete('/organisation', adminService.deleteOrganization);
 
 module.exports = router;
