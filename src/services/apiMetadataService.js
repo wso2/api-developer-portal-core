7ed20244a78201bcaed86b9b4233479ef3c7f041
sachinisiriwardene <sachinisiriwardene@gmail.com>
2024-10-07 12:19:20 +0530

    remove unwanted code

--- src/services/apiMetadataService.js
+++ src/services/apiMetadataService.js
@@ -53,95 +53,18 @@ const createAPIMetadata = async (req, res) => {
 };
 
 const getAPIMetadata = async (req, res) => {
-    try {
-        let param = req.query.orgId || req.query.orgName;
-
-        if (!param) {
-            throw new Sequelize.ValidationError("Missing required parameter: 'orgId' or 'orgName'");
-        }
-
-        const organization = await apiDao.getOrganization(param);
-
-        res.status(200).json({
-            orgId: organization.orgId,
-            orgName: organization.orgName,
-            authenticatedPages: organization.authenticatedPages.split(' ').filter(Boolean) // Convert back to array
-        });
-    } catch (error) {
-        util.handleError(res, error);
-    }
+    
 };
 
 const getAllAPIMetadata = async (req, res) => {
-    try {
-        let param = req.query.orgId || req.query.orgName;
-
-        if (!param) {
-            throw new Sequelize.ValidationError("Missing required parameter: 'orgId' or 'orgName'");
-        }
-
-        const organization = await apiDao.getOrganization(param);
-
-        res.status(200).json({
-            orgId: organization.orgId,
-            orgName: organization.orgName,
-            authenticatedPages: organization.authenticatedPages.split(' ').filter(Boolean) // Convert back to array
-        });
-    } catch (error) {
-        util.handleError(res, error);
-    }
+    
 };
 
 const updateAPIMetadata = async (req, res) => {
-    try {
-        let orgId = req.query.orgId;
-        const { orgName, authenticatedPages } = req.body;
 
-        if (!orgId) {
-            throw new Sequelize.ValidationError("Missing required parameter: 'orgId'");
-        }
-
-        if (!orgName || !authenticatedPages || !Array.isArray(authenticatedPages)) {
-            throw new Sequelize.ValidationError("Missing or Invalid fields in the request payload");
-        }
-
-        const authenticatedPagesStr = authenticatedPages.join(' ');
-        // Create organization in the database
-        const [updatedRowsCount, updatedOrg] = await apiDao.updateOrganization({
-            orgName,
-            authenticatedPages: authenticatedPagesStr,
-            orgId
-        });
-
-        res.status(200).json({
-            orgId: updatedOrg[0].dataValues.orgId,
-            orgName: updatedOrg[0].dataValues.orgName,
-            authenticatedPages: updatedOrg[0].dataValues.authenticatedPages.split(' ').filter(Boolean)
-        });
-
-    } catch (error) {
-        console.log(error);
-
-        util.handleError(res, error);
-    }
 };
 const deleteAPIMetadata = async (req, res) => {
-    try {
-        let orgId = req.query.orgId;
-
-        if (!orgId) {
-            throw new Sequelize.ValidationError("Missing required parameter: 'orgId'");
-        }
-
-        const deletedRowsCount = await apiDao.deleteOrganization(orgId);
-        if (deletedRowsCount > 0) {
-            res.status(204).send();
-        } else {
-            throw new Sequelize.EmptyResultError('Organization not foundw');
-        }
-    } catch (error) {
-        util.handleError(res, error);
-    }
+   
 };
 
 module.exports = {
