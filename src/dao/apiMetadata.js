0e357e9d5410518ef8ffc9380282f42dd491d0be
sachinisiriwardene <sachinisiriwardene@gmail.com>
2024-10-07 12:17:09 +0530

    remove unwanted code

--- src/dao/apiMetadata.js
+++ src/dao/apiMetadata.js
@@ -2,8 +2,6 @@ const { APIMetadata } = require('../models/apiMetadata');
 const ThrottlingPolicy = require('../models/throttlingPolicy');
 const AdditionalProperties = require('../models/additionalAPIProperties');
 const APIContent = require('../models/apiContent');
-
-const { validate } = require('uuid');
 const { Sequelize } = require('sequelize');
 
 const createAPIMetadata = async (apiMetadata, t) => {
@@ -100,74 +98,9 @@ const storeAPIDefinition = async (apiDefinition, fileName, apiID, t) => {
 }
 
 
-const getOrganization = async (param) => {
-    const whereCondition = validate(param) ? { orgId: param } : { orgName: param };
-    try {
-        const organization = await apiMetadata.Organization.findOne({ where: whereCondition });
-
-        if (!organization) {
-            throw new Sequelize.EmptyResultError('Organization not found');
-        }
-
-        return organization;
-    } catch (error) {
-        if (error instanceof Sequelize.EmptyResultError) {
-            throw error;
-        }
-        throw new Sequelize.DatabaseError(error);
-    }
-};
-
-const updateOrganization = async (orgData) => {
-
-    try {
-        const [updatedRowsCount, updatedOrg] = await apiMetadata.Organization.update(
-            {
-                orgName: orgData.orgName,
-                authenticatedPages: orgData.authenticatedPages
-            },
-            {
-                where: { orgId: orgData.orgId },
-                returning: true
-            }
-        );
-
-        if (updatedRowsCount < 1) {
-            throw new Sequelize.EmptyResultError('Organization not found');
-        } else {
-            return [updatedRowsCount, updatedOrg];
-        }
-    } catch (error) {
-        if (error instanceof Sequelize.EmptyResultError) {
-            throw error;
-        }
-        throw new Sequelize.DatabaseError(error);
-    }
-};
-
-const deleteOrganization = async (orgId) => {
-    try {
-        const deletedRowsCount = await apiMetadata.Organization.destroy({
-            where: { orgId }
-        });
-        if (deletedRowsCount < 1) {
-            throw Object.assign(new Sequelize.EmptyResultError('Organization not found'));
-        } else {
-            return deletedRowsCount;
-        }
-    } catch (error) {
-        if (error instanceof Sequelize.EmptyResultError) {
-            throw error;
-        }
-        throw new Sequelize.DatabaseError(error);
-    }
-}
 
 module.exports = {
     createAPIMetadata,
-    getOrganization,
-    updateOrganization,
-    deleteOrganization,
     createThrottlingPolicy,
     storeAdditionalProperties,
     storeAPIDefinition
