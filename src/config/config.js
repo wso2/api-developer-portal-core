8ec8101f7823ca4b13c680ee711dd337f30dbb04
Divya Premanantha <premananthadivya@gmail.com>
2024-11-13 13:04:27 +0530

    Update config

--- src/config/config.js
+++ src/config/config.js
@@ -7,7 +7,7 @@ config.mode = 'multi'
 config.db = {
     username: 'postgres',
     password: 'postgres',
-    database: 'dev',
+    database: 'devportal',
     host: 'localhost',
     dialect: 'postgres',
 };
