231dd9e77a46ae31279b73eabbe9bee97b4530f6
sachinisiriwardene <sachinisiriwardene@gmail.com>
2024-12-10 13:40:39 +0530

    load control plane api path from a constant

--- src/controllers/myAPIsController.js
+++ src/controllers/myAPIsController.js
@@ -24,6 +24,7 @@ const constants = require('../utils/constants');
 const adminDao = require('../dao/admin');
 const apiMetadataService = require('../services/apiMetadataService');
 const util = require('../utils/util');
+const controlPlaneUrl = config.controlPlane.url;
 
 const loadMyAPIs = async (req, res) => {
     try {
@@ -40,7 +41,7 @@ const loadMyAPIs = async (req, res) => {
         let subscribedApps = [];
 
         for (const apiRefId of apiRefIds) {
-            const subs = await loadSubscriptions(apiRefId);
+            const subs = await loadSubscriptions(req, apiRefId);
 
             if (subs) {
                 for (const sub of subs.list) {
@@ -61,7 +62,7 @@ const loadMyAPIs = async (req, res) => {
         let apiId = req.query.apiId;
         if (apiId) {
             const apps = await loadApplications(req);
-            const apiSubs = await loadSubscriptions(apiId.replace(/[^a-zA-Z0-9\s-]/g, ''));
+            const apiSubs = await loadSubscriptions(req, apiId.replace(/[^a-zA-Z0-9\s-]/g, ''));
             if (Array.isArray(apiSubs.list)) {
 
 
@@ -100,7 +101,7 @@ const loadMyAPIs = async (req, res) => {
 
 const loadSubscriptions = async (req, apiId) => {
     try {
-        return await util.invokeApiRequest(req, 'GET', `${config.controlPlaneUrl}/subscriptions?apiId=${apiId}`);
+        return await util.invokeApiRequest(req, 'GET', `${controlPlaneUrl}/subscriptions?apiId=${apiId}`);
     } catch (error) {
         console.error("Error occurred while loading subscriptions", error);
         throw error;
@@ -109,7 +110,7 @@ const loadSubscriptions = async (req, apiId) => {
 
 const loadApplications = async (req) => {
     try {
-        return await util.invokeApiRequest(req, 'GET', `${config.controlPlaneUrl}/applications?sortBy=name&sortOrder=asc`);
+        return await util.invokeApiRequest(req, 'GET', `${controlPlaneUrl}/applications?sortBy=name&sortOrder=asc`);
     } catch (error) {
         console.error("Error occurred while loading applications", error);
         throw error;
