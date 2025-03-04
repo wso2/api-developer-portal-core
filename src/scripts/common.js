d8af22272f4ab6d3b0c76ee99392bf712cbd17fc
Divya Premanantha <premananthadivya@gmail.com>
2025-03-04 21:03:46 +0530

    Remove console log

--- src/scripts/common.js
+++ src/scripts/common.js
@@ -36,10 +36,8 @@ document.addEventListener("DOMContentLoaded", function () {
     });
     
     const navLinks = document.querySelectorAll('.nav-link');
-    console.log(navLinks);
     navLinks.forEach(link => {
         link.addEventListener('click', function (event) {
-            console.log('clicked');
             navLinks.forEach(link => link.classList.remove('active'));
             event.currentTarget.classList.add('active');
         });
