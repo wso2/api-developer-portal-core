4288e33079c96d59016ba39d2f68ed788e09f917
Divya Premanantha <premananthadivya@gmail.com>
2025-03-07 12:08:19 +0530

    Fix issue

--- src/controllers/apiContentController.js
+++ src/controllers/apiContentController.js
@@ -402,7 +402,7 @@ async function loadAPIMetaData(req, orgID, apiID, viewName) {
     //replace image urls
     let images = metaData.apiInfo.apiImageMetadata;
     for (const key in images) {
-        apiImageUrl = `${config.advanced.resourceLoadFromBaseUrl ? config.baseUrl : `${req.protocol}://${req.get('host')}`}${constants.ROUTE.DEVPORTAL_ASSETS_BASE_PATH}${orgID}${constants.ROUTE.API_FILE_PATH}${element.apiID}${constants.API_TEMPLATE_FILE_NAME}`;
+        let apiImageUrl = `${config.advanced.resourceLoadFromBaseUrl ? config.baseUrl : `${req.protocol}://${req.get('host')}`}${constants.ROUTE.DEVPORTAL_ASSETS_BASE_PATH}${orgID}${constants.ROUTE.API_FILE_PATH}${element.apiID}${constants.API_TEMPLATE_FILE_NAME}`;
         const modifiedApiImageURL = apiImageUrl + images[key];
         element.apiInfo.apiImageMetadata[key] = modifiedApiImageURL;
     }
