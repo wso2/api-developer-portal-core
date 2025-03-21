4c3026f3df57629b0156a66037439d1acb816832
piyumaldk <piyumaldk@gmail.com>
2025-03-21 15:20:06 +0530

    Add loading to application deletion

--- src/scripts/delete-confirmation.js
+++ src/scripts/delete-confirmation.js
@@ -9,6 +9,7 @@ function openDeleteModal(param1, param2, param3, param4) {
 }
 
 async function deleteApplication() {
+  document.getElementById('loading-screen').style.display = 'block';
   const modal = document.getElementById('deleteConfirmation');
   const applicationId = modal.dataset.param1;
   try {
@@ -20,9 +21,13 @@ async function deleteApplication() {
       console.error('Failed to delete application.');
       await showAlert('Failed to delete application. Please try again.', 'error');
     }
+    window.onload = function() {
+      document.getElementById('loading-screen').style.display = 'none';
+    };
   } catch (error) {
     console.error('Error deleting application:', error);
     await showAlert('An error occurred while deleting the application. Please try again.', 'error');
+    document.getElementById('loading-screen').style.display = 'none';
   }
   window.location.reload(true);
 }
