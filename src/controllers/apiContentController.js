2fae605419f41060c1705129a9acd10596a40dc4
sachinisiriwardene <sachinisiriwardene@gmail.com>
2025-03-25 10:42:26 +0530

    remove appending basePath when fetching assets

--- src/controllers/apiContentController.js
+++ src/controllers/apiContentController.js
@@ -413,7 +413,7 @@ async function loadAPIMetaData(req, orgID, apiID, viewName) {
         //replace image urls
         let images = metaData.apiInfo.apiImageMetadata;
         for (const key in images) {
-            let apiImageUrl = `${config.advanced.resourceLoadFromBaseUrl ? config.baseUrl : `${req.protocol}://${req.get('host')}`}${constants.ROUTE.DEVPORTAL_ASSETS_BASE_PATH}${orgID}${constants.ROUTE.API_FILE_PATH}${apiID}${constants.API_TEMPLATE_FILE_NAME}`;
+            let apiImageUrl = `${constants.ROUTE.DEVPORTAL_ASSETS_BASE_PATH}${orgID}${constants.ROUTE.API_FILE_PATH}${apiID}${constants.API_TEMPLATE_FILE_NAME}`;
             const modifiedApiImageURL = apiImageUrl + images[key];
             images[key] = modifiedApiImageURL;
         }
