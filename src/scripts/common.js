f57658784cf3c983da5f7f9d599d3d56064fafb0
sachinisiriwardene <sachinisiriwardene@gmail.com>
2025-03-28 08:29:20 +0530

    revert change to select app on redirection

--- src/scripts/common.js
+++ src/scripts/common.js
@@ -190,15 +190,14 @@ document.addEventListener("DOMContentLoaded", function () {
                 let params = new URLSearchParams(window.location.search);
                 let appId, appName = "";
                 // Get application data
-                if (params.has('appID')) {
-                     appId = params.get('appID');
-                     appName  = params.get('appName');
-                } else {
+                // if (params.has('appID')) {
+                //      appId = params.get('appID');
+                //      appName  = params.get('appName');
+                // }
+                if (appId && appName) {
                     const firstAvailableApp = dropdown.querySelector(".select-item:not(.disabled)");
                     appId = firstAvailableApp.getAttribute("data-value");
                     appName = firstAvailableApp.getAttribute("data-app-name");
-                }
-                if (appId && appName) {
                     // Update hidden input with selected app ID
                     const hiddenField = document.getElementById(
                         dropdown.querySelector("[id^='selectedAppId-']").id
