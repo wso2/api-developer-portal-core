2397beb0637d4e86edc7a9180b8ce108af9e961b
piyumaldk <piyumaldk@gmail.com>
2025-06-06 15:58:44 +0530

    Fix issues and add a default wrapper

--- src/utils/telemetryClient.js
+++ src/utils/telemetryClient.js
@@ -1,30 +1,11 @@
-import appInsights from 'applicationinsights';
-
+// process.env.APPLICATIONINSIGHTS_LOGGING_LEVEL = 'verbose';
+const appInsights = require('applicationinsights');
 const secret = require(process.cwd() + '/secret.json');
-const azureInsightsKey = secret.azureInsightsKey;
 
-appInsights.setup(azureInsightsKey)
-    .setAutoDependencyCorrelation(true)
-    .setAutoCollectRequests(true)
-    .setAutoCollectPerformance(true)
-    .setAutoCollectExceptions(true)
-    .setAutoCollectDependencies(true)
-    .setAutoCollectConsole(true)
-    .setUseDiskRetryCaching(true)
-    .start();
+appInsights.setup(secret.azureInsightsKey);
 
 const telemetryClient = appInsights.defaultClient;
 
-telemetryClient.addTelemetryProcessor((envelope) => {
-    if (!envelope.data) return;
-    if (!envelope.data.baseData) return;
-
-    if (!envelope.data.baseData.properties) {
-        envelope.data.baseData.properties = {};
-    }
-
-    envelope.data.baseData.properties.context = 'devportal';
-    envelope.data.baseData.properties.origin = 'bijira';
-});
-
-export { telemetryClient };
+module.exports = {
+    telemetryClient
+};
