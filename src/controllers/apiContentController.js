6e4f42a8885ecfa641caea7ef462083f05b87f85
Thushani Jayasekera <thushanij.18@cse.mrt.ac.lk>
2025-10-03 12:15:39 +0530

    Fix type issue

--- src/controllers/apiContentController.js
+++ src/controllers/apiContentController.js
@@ -269,18 +269,35 @@ const loadAPIContent = async (req, res) => {
                 let additionalAPIContentResponse = await apiDao.getAPIFile(constants.FILE_NAME.API_HBS_CONTENT_FILE_NAME, constants.DOC_TYPES.API_LANDING, orgID, apiID);
                 if (!additionalAPIContentResponse) {
                     loadDefault = true;
-                    if (metaData.apiInfo && metaData.apiInfo.apiType !== "GraphQL" && metaData.apiInfo.apiType !== constants.API_TYPE.WS) {
-                        apiDefinition = "";
-                        apiDefinition = await apiDao.getAPIFile(constants.FILE_NAME.API_DEFINITION_FILE_NAME, constants.DOC_TYPES.API_DEFINITION, orgID, apiID);
-                        apiDefinition = apiDefinition.API_FILE.toString(constants.CHARSET_UTF8);
-                        apiDetails = await parseSwagger(JSON.parse(apiDefinition))
-                        if (metaData.endPoints.productionURL === "" && metaData.endPoints.sandboxURL === "") {
-                            apiDetails["serverDetails"] = "";
-                        } else {
-                            apiDetails["serverDetails"] = metaData.endPoints;
-                        }
+                    if (
+                      metaData.apiInfo &&
+                      metaData.apiInfo.apiType !== "GraphQL" &&
+                      metaData.apiInfo.apiType !== "AsyncAPI" &&
+                      metaData.apiInfo.apiType !== "WS"
+                    ) {
+                      apiDefinition = "";
+                      apiDefinition = await apiDao.getAPIFile(
+                        constants.FILE_NAME.API_DEFINITION_FILE_NAME,
+                        constants.DOC_TYPES.API_DEFINITION,
+                        orgID,
+                        apiID
+                      );
+                      apiDefinition = apiDefinition.API_FILE.toString(
+                        constants.CHARSET_UTF8
+                      );
+                      apiDetails = await parseSwagger(
+                        JSON.parse(apiDefinition)
+                      );
+                      if (
+                        metaData.endPoints.productionURL === "" &&
+                        metaData.endPoints.sandboxURL === ""
+                      ) {
+                        apiDetails["serverDetails"] = "";
+                      } else {
+                        apiDetails["serverDetails"] = metaData.endPoints;
+                      }
                     }
-                    if (metaData.apiInfo.apiType === constants.API_TYPE.WS) {
+                    if (metaData.apiInfo.apiType === "AsyncAPI" || metaData.apiInfo.apiType === "WS") {
                         apiDefinition = "";
                         apiDefinition = await apiDao.getAPIFile(constants.FILE_NAME.API_DEFINITION_FILE_NAME, constants.DOC_TYPES.API_DEFINITION, orgID, apiID);
                         apiDefinition = apiDefinition.API_FILE.toString(constants.CHARSET_UTF8);
