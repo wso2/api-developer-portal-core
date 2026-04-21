c0b0113ca0da53ddd2c4a14aba7970c1a09de56b
Thushani Jayasekera <thushanij.18@cse.mrt.ac.lk>
2026-04-21 23:37:48 +0530

    refactor: update DP_API_FLOW table schema to remove unique constraint on HANDLE and add composite unique index for ORG_ID, VIEW_ID, and HANDLE

--- src/models/apiFlow.js
+++ src/models/apiFlow.js
@@ -43,8 +43,7 @@ const APIFlow = sequelize.define('DP_API_FLOW', {
     },
     HANDLE: {
         type: DataTypes.STRING,
-        allowNull: true,
-        unique: true
+        allowNull: true
     },
     AGENT_PROMPT: {
         type: DataTypes.TEXT,
@@ -90,7 +89,15 @@ const APIFlow = sequelize.define('DP_API_FLOW', {
 }, {
     timestamps: false,
     tableName: 'DP_API_FLOW',
-    returning: true
+    returning: true,
+    indexes: [
+        {
+            unique: true,
+            fields: ['ORG_ID', 'VIEW_ID', 'HANDLE'],
+            where: { HANDLE: { [Sequelize.Op.ne]: null } },
+            name: 'IDX_DP_API_FLOW_ORG_VIEW_HANDLE'
+        }
+    ]
 });
 
 APIFlow.belongsTo(Organization, { foreignKey: 'ORG_ID' });
