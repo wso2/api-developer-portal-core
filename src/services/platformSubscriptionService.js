df937eb8f4c0b82f2bc52afe4dcab9151dc00db1
Piumal Rathnayake <piumal1999@gmail.com>
2026-05-19 12:00:58 +0530

    Fix typo in subscription policies key

--- src/services/platformSubscriptionService.js
+++ src/services/platformSubscriptionService.js
@@ -89,7 +89,7 @@ const createPlatformGatewaySubscription = async (req, res) => {
             });
         }
 
-        const plans = apiMetadata.SubscriptionPolicies || [];
+        const plans = apiMetadata.DP_SUBSCRIPTION_POLICies || [];
         if (plans.length === 0) {
             return res.status(400).json({
                 code: '400', message: 'Bad Request',
